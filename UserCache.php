81a6b2828e3071284fcab6b58c8e4f915e4aa319
ahprinters <ahps323@gmail.com>
2025-03-03 23:06:57 +0600

    Modify codes

--- UserCache.php
+++ UserCache.php
@@ -8,7 +8,7 @@ class UserCache {
     }
 }
 $user = new UserCache('John', 30);
-// file_put_contents('cache.txt', serialize($user));
-$cachedUser= unserialize(file_get_contents('cache.txt'));
-// echo $cachedUser->name . ' is ' . $cachedUser->age . ' years old.';
-var_dump($cachedUser);
+file_put_contents('cache.txt', serialize($user)); // Save the serialized object to a file
+$cachedUser = unserialize(file_get_contents('cache.txt')); // Read and unserialize the object from the file
+echo $cachedUser->name . ' is ' . $cachedUser->age . ' years old.'; // Print the object's properties
+var_dump($cachedUser); // Print the details of the object
